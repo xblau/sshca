9119dd41b6894a80af1ab3e24f3ae66377d032c1
Daniel Mosquera <daniel@blaudev.es>
2018-04-03 13:40:35 +0200

    First commit

---
+++ config.sh
@@ -0,0 +1,15 @@
+bin_path='/usr/bin/ssh-keygen'
+
+ca_key_type='ed25519'
+ca_key_size='256'
+ca_key_path='keys/ssh_ca_key'
+
+ca_smartcard='false'
+ca_smartcard_lib='/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so'
+
+key_hash='sha256'
+key_types='rsa,ed25519'
+
+cert_validity='90d'
+
+remote_user='root'
